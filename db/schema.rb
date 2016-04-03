8e1657e1b3e5d2f7cb7eb5707942d9fc0855ffb8
yamaohiroshi <aoiumi123@yahoo.co.jp>
2016-04-03 08:11:56 +0000

    Gemfile update for heroku1

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20160320195858) do
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
+    t.string   "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
