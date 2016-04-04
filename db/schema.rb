1bb9d66f90cd6bfeaa81de7c62f85986d6a7f873
yamaohiroshi <aoiumi123@yahoo.co.jp>
2016-04-04 12:42:35 +0000

    Challnge2

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320195858) do
+ActiveRecord::Schema.define(version: 20160331193057) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
-    t.string   "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
